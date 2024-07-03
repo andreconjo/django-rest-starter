7b41b49d4ec65cd60121a71ef02c70095f4716cf
João Marcelo Saraiva Figueira <joao.marcelo@teclat.com.br>
2024-07-02 23:48:06 -0300

    cargo

--- orcamentoComercial/admin.py
+++ orcamentoComercial/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from .forms import OrcamentoComercialForm
-from .models import OrcamentoComercial
+from .models import OrcamentoComercial, Cargo
 
 @admin.register(OrcamentoComercial)
 class OrcamentoComercialAdmin(admin.ModelAdmin):
     form = OrcamentoComercialForm
-    list_display = ('area', 'profissional', 'senioridade', 'forma_pagamento', 'margem')
-    list_filter = ('area', 'profissional', 'senioridade', 'forma_pagamento')
-    search_fields = ('area', 'profissional', 'senioridade', 'forma_pagamento')
+    list_display = ('area', 'cargo','forma_pagamento', 'margem')
+    list_filter = ('area', 'cargo','forma_pagamento')
+    search_fields = ('area', 'cargo','forma_pagamento')
+
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'senioridade','salario')
 
--- orcamentoComercial/models.py
+++ orcamentoComercial/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Cargo(models.Model):
+    nome = models.CharField(max_length=40)
+    senioridade = models.CharField(max_length=40)
+    salario = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"{self.nome}"
+
 class OrcamentoComercial(models.Model):
     AREA_CHOICES = [
         ('analytics', 'Analytics'),
@@ -7,19 +15,19 @@ class OrcamentoComercial(models.Model):
         ('gestao', 'Gestão'),
     ]
 
-    PROFISSIONAL_CHOICES = [
-        ('desenvolvedor', 'Desenvolvedor'),
-        ('analista_dados', 'Analista de Dados'),
-    ]
+    #PROFISSIONAL_CHOICES = [
+    #    ('desenvolvedor', 'Desenvolvedor'),
+    #    ('analista_dados', 'Analista de Dados'),
+    #]
 
-    SENIORIDADE_CHOICES = [
-        ('junior', 'Junior'),
-        ('pleno', 'Pleno'),
-        ('senior', 'Senior'),
-        ('especialista', 'Especialista'),
-        ('coordenador', 'Coordenador'),
-        ('tech_lead', 'Tech Lead'),
-    ]
+    #SENIORIDADE_CHOICES = [
+    #    ('junior', 'Junior'),
+    #    ('pleno', 'Pleno'),
+    #    ('senior', 'Senior'),
+    #    ('especialista', 'Especialista'),
+    #    ('coordenador', 'Coordenador'),
+    #    ('tech_lead', 'Tech Lead'),
+    #]
 
     FORMA_PAGAMENTO_CHOICES = [
         ('pre_pago', 'Pré Pago'),
@@ -29,12 +37,11 @@ class OrcamentoComercial(models.Model):
     ]
 
     area = models.CharField(max_length=40, choices=AREA_CHOICES)
-    profissional = models.CharField(max_length=40, choices=PROFISSIONAL_CHOICES)
-    senioridade = models.CharField(max_length=40, choices=SENIORIDADE_CHOICES)
+    cargo = models.ForeignKey(Cargo, on_delete=models.DO_NOTHING)
     forma_pagamento = models.CharField(max_length=40, choices=FORMA_PAGAMENTO_CHOICES)
     margem = models.DecimalField(max_digits=5, decimal_places=2, default=10.00)
 
     def __str__(self):
-        return f"{self.area} - {self.profissional} - {self.senioridade}"
+        return f"{self.area} - {self.cargo} - {self.cargo.nome}"
 
 
