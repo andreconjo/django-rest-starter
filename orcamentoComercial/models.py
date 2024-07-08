df6950e8c0e80f408f6ed8530b9f09bad45ce782
João Marcelo Saraiva Figueira <joao.marcelo@teclat.com.br>
2024-07-07 22:28:29 -0300

    new models

--- orcamentoComercial/models.py
+++ orcamentoComercial/models.py
@@ -21,7 +21,7 @@ class Senioridade(models.Model):
 class Cargo(models.Model):
     nome = models.CharField(max_length=40)
     senioridade = models.ForeignKey(Senioridade, on_delete=models.CASCADE)
-    salario = models.CharField(max_length=40)
+    salario = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"{self.nome}"
